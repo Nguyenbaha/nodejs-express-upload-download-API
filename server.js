0939c407b5ca185820bb163ab5d75078483d58cf
unknown <nbhafithou@gmail.com>
2022-03-08 21:18:00 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+//
+// run app express
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+global.__basedir = __dirname;
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+const initRoutes = require("./src/routes");
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+let port = 8085;
+app.listen(port, () => {
+    console.log(`Running at localhost:${port}`);
+});
